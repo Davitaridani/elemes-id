d3ef876f89f285a2831da2706600abff7cb58676
Davitaridani <davitaridani@gmail.com>
2023-06-09 14:32:24 +0700

    setup project

---
+++ src/sections/CarouselCategory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CarouselCategory = () => {
+  return (
+    <section className="sect-carousel">
+      <div class="container"></div>
+    </section>
+  );
+};
+
+export default CarouselCategory;
---
+++ src/sections/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Products = () => {
+  return (
+    <section className="sect-producs">
+      <Container></Container>
+    </section>
+  );
+};
+
+export default Products;
